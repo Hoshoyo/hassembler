9ce454703ef6ac2446a30fbc9c1ba666859d6586
Hoshoyo <hoshoyo@gmail.com>
2022-04-09 18:19:27 -0300

    Fix build.bat

--- instruction_tests/build.bat
+++ instruction_tests/build.bat
@@ -1,13 +1,13 @@
 @echo off
 
 pushd bin
-REM cl /Zi /nologo ../checker.c
+cl /Zi /nologo ../checker.c
 
-REM cl /Zi /nologo /I../.. ../test_mul.c ../../*.c
-REM cl /Zi /nologo /I../.. ../test_arith.c ../../*.c
-REM cl /Zi /nologo /I../.. ../test_mov.c ../../*.c
-REM cl /Zi /nologo /I../.. ../test_shift.c ../../*.c
-REM cl /Zi /nologo /I../.. ../test_jmp.c ../../*.c
+cl /Zi /nologo /I../.. ../test_mul.c ../../*.c
+cl /Zi /nologo /I../.. ../test_arith.c ../../*.c
+cl /Zi /nologo /I../.. ../test_mov.c ../../*.c
+cl /Zi /nologo /I../.. ../test_shift.c ../../*.c
+cl /Zi /nologo /I../.. ../test_jmp.c ../../*.c
 cl /Zi /nologo /I../.. ../test_sse.c ../../*.c
 
 popd
